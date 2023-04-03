7352f6017b900fe7923f3bb3eb46339b271bf989
shomyich <220009@astanait.edu.kz>
2023-04-03 15:40:05 +0600

    scanner added

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = scanner.nextInt();
         }
         System.out.println(findMin(arr, n));
     }
